e8caa649444a98298e900382a54dcfd730dc2f39
hellosamzo <samuelmatthews96@gmail.com>
2018-03-07 21:16:55 +0000

    Messing about with calling Python scripts

--- S.N.T/src/Main.java
+++ S.N.T/src/Main.java
@@ -6,12 +6,14 @@
  * Sam's Networking Tool - CLI Version
  */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.io.IOException;
-import java.net.SocketException;
+//import org.apache.log4j.*;
 
 
 public class Main {
@@ -30,13 +32,42 @@ public class Main {
 		}
 	}
 
+	public Main(String choice) {
+		switch (choice) {
+		case "m":
+			manual();
+			break;
+		}
+	}
+	
+	public Main(int choice) throws IOException {
+		switch (choice) {
+		case 4:
+			py();
+			break;
+		}
+	}
+
+	private void py() throws IOException {
+		ProcessBuilder builder = new ProcessBuilder(
+	            "cmd.exe", "/c", "cd C:\\Users\\User\\git\\SNT\\S.N.T\\Scripts && test.py");
+	        builder.redirectErrorStream(true);
+	        Process p = builder.start();
+	        BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+	        String line;
+	        while (true) {
+	            line = r.readLine();
+	            if (line == null) { break; }
+	            System.out.println(line);
+	}
+}
+
 	private void NSLookup(String value) {
 		try {
 			InetAddress addressHost = InetAddress.getByName(value);
 			String domain = addressHost.getHostName();
-			// System.out.println("The host name was: " + hostName);
-			System.out.println("The IP address for host " + domain + ": " + addressHost.getHostAddress());
-
+			System.out.println("HOST NAME: " + domain);
+			System.out.println("IP ADDRESS: " + addressHost.getHostAddress());
 		} catch (UnknownHostException ex) {
 			System.out.println("Can't find host");
 		}
@@ -46,6 +77,7 @@ public class Main {
 	private void portScanner(String value) {
 		// check all 1 -> 65,535 ports
 		for (int port = 1; port <= 65535; port++) {
+			// to do - place log4j debug here
 	         try {
 	            Socket socket = new Socket();
 	            socket.connect(new InetSocketAddress("localhost", port), 1000);
@@ -73,10 +105,21 @@ public class Main {
 		}
 	}
 
+	private void manual() {
+		System.out.println("\nNS LOOKUP - Enter either an IP Address or a host name to retrieve both back.");
+		System.out.println("PING - Test connectivity between your machine and the machine you're pinging.");
+	}
+	
 	// choice to user, provide testing for args
-	public static void main(String[] args) {
-		System.out.println("1 FOR NSLOOKUP\n ");
-		new Main(1, "cnn.com");
-		new Main(3, "127.0.0.1");
+	public static void main(String[] args) throws IOException {
+		System.out.println("SAM'S NETWORKING TOOL V1.1\n\nOptions:");
+		System.out.println("1 - NSLOOKUP");
+		System.out.println("2 - PORT SCANNER");
+		System.out.println("3 - PING");
+		System.out.println("M - Manual\n");
+		//new Main(1, "cnn.com");
+		new Main(4);
+	//	new Main(2, "localhost");
+	//	new Main(3, "8.8.8.8");
 	}
 }
